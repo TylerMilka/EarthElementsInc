5b72cb5ea31a32f5647709ced557880e77c9e9e5
Tyler <tgmilka@gmail.com>
2017-01-23 00:52:45 -0600

    Must fix nav bar for mobile view

---
+++ app.js
@@ -0,0 +1,23 @@
+// //Code Starts
+// $(document).ready(function() {
+//     $("#hover").css("opacity", 0.5);
+//     $("#hover").hover(function() {
+//         $(this).animate({opacity: 1.0}, 500);
+//     }, function() {
+//         $(this).animate({opacity: 0.5}, 500);
+//     });
+
+// var rotation = 0;
+
+// jQuery.fn.rotate = function(degrees) {
+//     $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
+//     return $(this);
+// };
+
+// $('#.logo').hover(function() {
+//     rotation += 90;
+//     $(this).rotate(rotation);
+
+// });
+
+// })
